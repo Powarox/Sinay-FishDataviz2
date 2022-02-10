e98c4ae6ad8ff21556dbf3fd803b45ed6f8248ec
Powarox219 <moriniere.robin1877@gmail.com>
2022-02-10 16:19:17 +0100

    Add route to components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component'
+import { ListeEspecesComponent } from './liste-especes/liste-especes.component'
+
+const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'liste-especes', component: ListeEspecesComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
